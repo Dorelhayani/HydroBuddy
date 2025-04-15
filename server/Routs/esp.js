ce6d6753840f5def6874d17bb585ff3c6187b1c2
dorel <dorelhayani@gmail.com>
2025-04-15 23:55:13 +0300

    son

--- server/Routs/esp.js
+++ server/Routs/esp.js
@@ -10,7 +10,7 @@ router.patch('/state', (req, res) => {
     try {
         let data = JSON.parse(fs.readFileSync("Inside_Information.json", "utf8"));
 
-        if (req.body.state) {
+        if (req.body.state ) {
             data.state = req.body.state;
             fs.writeFileSync("Inside_Information.json", JSON.stringify(data, null, 2), "utf8");
             console.log(`State updated to: ${data.state}`);
@@ -56,6 +56,31 @@ router.patch('/moisture', (req,res)=>{
         return res.json({message: "Moisture Mode Updated", })
     }catch (error){ res.status(500).json({ error: "Error handling moisture mode" }); }
 })
+router.patch('/Saturday', (req,res)=>{
+    try{
+        let timeAct = req.body.timeActivation;
+        let duration = parseInt(req.body.duration);
+        let splt = timeAct.split(':');
+        let hour = splt[0];
+        let minute = splt[1];
+        let ValidTime = (
+            hour >= "0" && hour <= "23" &&
+            minute >= "0" && minute <= "59" &&
+            duration > 0);
+        let data = JSON.parse(fs.readFileSync("Inside_Information.json", 'utf8'));
+        if(ValidTime){
+            data.SATURDAY_MODE={
+                timeAct,
+                duration
+            }
+            fs.writeFileSync("Inside_Information.json", JSON.stringify(data, null, 2), "utf8");
+            console.log(`Saturday mode set to: ${data.SATURDAY_MODE.timeAct} for ${data.SATURDAY_MODE.duration} `)
+            return res.json({ message: "Saturday mode updated", SATURDAY_MODE: data.SATURDAY_MODE });
+        }
+        return res.status(400).json({ error: "Please Insert a Valid Time Signature"});
+    }catch (error){ res.status(500).json({ error: "Error handling Saturday mode" }); }
+});
+
 router.patch('/manual', (req,res)=>{
     try{
         let data = JSON.parse(fs.readFileSync("Inside_Information.json", 'utf8'));
@@ -69,28 +94,5 @@ router.patch('/manual', (req,res)=>{
     }
     catch (error){ res.status(500).json({ error: "Error handling manual mode" }); }
 })
-router.patch('/Saturday', (req,res)=>{
-    try{
-        let hour = parseInt(req.body.hour);
-        let minute = parseInt(req.body.minute);
-        let duration = parseInt(req.body.duration);
-        let ValidTime = (Number.isInteger(hour) && Number.isInteger(minute) && Number.isInteger(duration)
-            &&  hour >= 0 && hour <= 23 &&
-            minute >= 0 && minute <= 59 &&
-            duration > 0);
 
-        let data = JSON.parse(fs.readFileSync("Inside_Information.json", 'utf8'));
-        if(ValidTime){
-            data.SATURDAY_MODE={
-                hour,
-                minute,
-                duration
-            }
-            fs.writeFileSync("Inside_Information.json", JSON.stringify(data, null, 2), "utf8");
-            console.log(`Saturday mode set to: ${data.SATURDAY_MODE.hour} : ${data.SATURDAY_MODE.minute} for ${data.SATURDAY_MODE.duration} `)
-            return res.json({ message: "Saturday mode updated", SATURDAY_MODE: data.SATURDAY_MODE });
-        }
-        return res.status(400).json({ error: "Please Insert a Valid Time Signature"});
-    }catch (error){ res.status(500).json({ error: "Error handling Saturday mode" }); }
-});
 module.exports = router;
