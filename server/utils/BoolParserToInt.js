b9c4fb2e2c23115e2feefd6371b27c209560af51
Dorelhayani <dorelhayani@gmail.com>
2025-10-20 15:54:23 +0300

    file name changing

---
+++ server/utils/BoolParserToInt.js
@@ -0,0 +1,13 @@
+// toInt01
+
+function toInt01(v) {
+    if (v === 1 || v === '1') return 1;
+    if (v === 0 || v === '0') return 0;
+
+    if (v === true || v === 'true' || v === 'TRUE') return 1;
+    if (v === false || v === 'false' || v === 'FALSE' || v == null || v === '') return 0;
+    const n = Number(v);
+    if (Number.isFinite(n)) return n ? 1 : 0;
+    return 0;
+}
+module.exports = { toInt01 };
